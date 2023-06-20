ce8cc5c221a4522f9421371a5a6def6d02e7af6a
Jahrulnr <jahrulnr@gmail.com>
2023-06-20 21:04:00 +0700

    update cors

--- server.js
+++ server.js
@@ -9,19 +9,22 @@ const ApiServerException = require('./app/exceptions/ApiServerError');
 const app = express();
 dotenv.config();
 
+const corsOpts = {
+  origin: '*',
+  methods: 'GET,OPTIONS,POST',
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true,
+  optionsSuccessStatus: 204
+}
+
 app.use(express.json());
-app.use(cors());
-app.options("*", cors());
+app.use(cors(corsOpts));
+app.options("*", cors(corsOpts));
 const port = Number(process.env.PORT || 3331);
 app.use('/api', apiRouter);
 app.use('/', webRouter);
 
 app.use(ApiServerException);
-app.use((res, req, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next()
-})
 app.all('/api/*', (req, res, next) => {
   const err = new ApiHttpException(404, 'Endpoint Not Found');
   console.log(err)
